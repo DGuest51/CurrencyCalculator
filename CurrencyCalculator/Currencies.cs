5ce4944076b93be5a06d99c3e71b553e66f4c6ab
Donovan Guest <Donovan.Guest@stmartin.edu>
2022-06-28 17:20:18 -0700

    Update

---
+++ CurrencyCalculator/Currencies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyCalculator
+{
+    public class Currencies
+    {
+        private string currencyName;
+        private string currencyCode;
+        private decimal ;
+
+    }
+}
--- CurrencyCalculator/Program.cs
+++ CurrencyCalculator/Program.cs
@@ -1,24 +1,26 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace FinalProject
 {
-    class CurrencyCalculator
+    public class CurrencyCalculator
     {
-        public static double DoOperation(double num1, double num2, string op)
+        public static double ConvertCurrency(string currencyFrom, string currencyTo, double currencyAmount)
         {
             double result = double.NaN; // Default value is "not-a-number" which we use if an operation, such as division, could result in an error.
 
             // Use a switch statement to do the math.
-            switch (op)
+            switch (currencyFrom,currencyTo)
             {
-                case "a":
-                    result = num1 + num2;
+                case "USD","EUR":
+                    result = currencyAmount * someNum;
                     break;
-                case "s":
-                    result = num1 - num2;
+                case "USDGBP":
+                    result = currencyAmount * someNum;
                     break;
-                case "m":
-                    result = num1 * num2;
+                case "USDCAD":
+                    result = currencyAmount * someNum;
                     break;
                 case "d":
                     // Ask the user to enter a non-zero divisor.
@@ -37,68 +39,110 @@ namespace FinalProject
     class Program
     {
         static void Main(string[] args)
+        {   
+            Console.WriteLine("C# CURRENCY CALCULATOR");
+            Console.WriteLine("----------------------");
+            Console.WriteLine();
+            Console.WriteLine("AVAILABLE CURRENCIES:");
+            Console.WriteLine();
+            var CurrencyList = new Dictionary<string, string>()
         {
-            bool endApp = false;
-            // Display title as the C# console calculator app.
-            Console.WriteLine("Currency Calculator in C#\r");
-            Console.WriteLine("------------------------\n");
+            {"United States Dollar", "USD"},
+            {"The EURO", "EUR"},
+            {"British Pound Sterling", "GBP"},
+            {"Canadian Dollar", "CAD"},
+            {"Australian Dollar", "AUD"},
+            {"Chinese Yuan", "CNY"},
+            {"Japanese Yen", "JPY"},
+            {"Hong Kong Dollar", "HKD"},
+        };
 
-            while (!endApp)
+            foreach (var currency in CurrencyList)
             {
-                // Declare variables and set to empty.
-                string strInput1 = "";
-                string strInput2 = "";
-                double result = 0;
-
-                // Ask the user to type the FROM currency
-                Console.Write("Type what currency you want to convert FROM: ");
-                strInput1 = Console.ReadLine();
-
-                //double cleanNum1 = 0;
-                //while (!double.TryParse(strInput1, out cleanNum1))
-                //{
-                //    Console.Write("This is not valid currency abbreviation. Please enter a three-letter currency abbreviation: ");
-                //    strInput1 = Console.ReadLine();
-                //}
-
-                // Ask the user to type the TO currency.
-                Console.Write("Type what currency you want to convert TO: ");
-                strInput2 = Console.ReadLine();
-
-                //double cleanNum2 = 0;
-                //while (!double.TryParse(strInput2, out cleanNum2))
-                //{
-                //    Console.Write("This is not valid currency abbreviation. Please enter a three-letter currency abbreviation: ");
-                //    strInput2 = Console.ReadLine();
-                //}
-
-                // Ask the user to choose an operator.
-
-                string op = Console.ReadLine();
-
-                try
-                {
-                    result = CurrencyCalculator.DoOperation(cleanNum1, cleanNum2, op);
-                    if (double.IsNaN(result))
-                    {
-                        Console.WriteLine("This operation will result in a mathematical error.\n");
-                    }
-                    else Console.WriteLine("Your result: {0:0.##}\n", result);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
-                }
-
-                Console.WriteLine("------------------------\n");
-
-                // Wait for the user to respond before closing.
-                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-                if (Console.ReadLine() == "n") endApp = true;
-
-                Console.WriteLine("\n"); // Friendly linespacing.
+                Console.WriteLine($"\t{currency.Key}: {currency.Value}");
             }
-            return;
+            Console.WriteLine();
+
+            string currencyFrom;
+            string currencyTo;
+            double currencyAmount;
+
+            Console.WriteLine("Enter three-letter currency code you want to convert FROM: ");
+            currencyFrom = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Enter three-letter currency code you want to convert TO: ");
+            currencyTo = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Enter numerical amount you want to convert: ");
+            currencyAmount = Convert.ToDouble(Console.ReadLine());
+
+            //bool CurrencyListHasCurrency1 = CurrencyList.ContainsValue(currencyCode1);
+            
+
+            //    bool endApp = false;
+            //    // Display title as the C# console calculator app.
+            //    Console.WriteLine("Currency Calculator in C#\r");
+            //    Console.WriteLine("------------------------\n");
+
+            //    while (!endApp)
+            //    {
+            //        // Declare variables and set to empty.
+            //        string strInput1 = "";
+            //        string strInput2 = "";
+            //        double result = 0;
+
+            //        // Ask the user to type the FROM currency
+            //        Console.Write("Type what currency you want to convert FROM: ");
+            //        strInput1 = Console.ReadLine();
+
+            //        //double cleanNum1 = 0;
+            //        //while (!double.TryParse(strInput1, out cleanNum1))
+            //        //{
+            //        //    Console.Write("This is not valid currency abbreviation. Please enter a three-letter currency abbreviation: ");
+            //        //    strInput1 = Console.ReadLine();
+            //        //}
+
+            //        // Ask the user to type the TO currency.
+            //        Console.Write("Type what currency you want to convert TO: ");
+            //        strInput2 = Console.ReadLine();
+
+            //        //double cleanNum2 = 0;
+            //        //while (!double.TryParse(strInput2, out cleanNum2))
+            //        //{
+            //        //    Console.Write("This is not valid currency abbreviation. Please enter a three-letter currency abbreviation: ");
+            //        //    strInput2 = Console.ReadLine();
+            //        //}
+
+            //        // Ask the user to choose an operator.
+
+            //        string op = Console.ReadLine();
+
+            //        try
+            //        {
+            //            result = CurrencyCalculator.DoOperation(cleanNum1, cleanNum2, op);
+            //            if (double.IsNaN(result))
+            //            {
+            //                Console.WriteLine("This operation will result in a mathematical error.\n");
+            //            }
+            //            else Console.WriteLine("Your result: {0:0.##}\n", result);
+            //        }
+            //        catch (Exception e)
+            //        {
+            //            Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+            //        }
+
+            //        Console.WriteLine("------------------------\n");
+
+            //        // Wait for the user to respond before closing.
+            //        Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            //        if (Console.ReadLine() == "n") endApp = true;
+
+            //        Console.WriteLine("\n"); // Friendly linespacing.
+            //    }
+            //    return;
+            //}
         }
     }
 }
